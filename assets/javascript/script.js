e2a89a558a0041cf03677161a9111ad9aa9a078f
Tcx7 <98015500+Tcx7@users.noreply.github.com>
2022-02-21 03:11:24 -0500

    added edit button

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,18 +1,22 @@
 const addTodoList = document.getElementById("toDoList"); //variable to fetch toDoList from HTML div
 const compList = document.getElementById("compList");
+var i = 0;
+
 document.getElementById("submit-btn").addEventListener("click", btnClicked);
 
 function btnClicked() {
   var userInput = document.getElementById("userInput");
   var divText = document.createTextNode(userInput.value); //variable to create the value from user input
+  var createDiv = document.createElement("li"); //variable to create <div></div>
+  createDiv.classList.add(`list-${i}`);
   const delBtn = document.createElement("button"); //variable to create element for button
   const compBtn = document.createElement("button");
+  const editBtn = document.createElement("button");
   delBtn.addEventListener("click", (e) => {
     //(e)is mouse event!!!
     addTodoList.removeChild(createDiv);
   });
   compBtn.addEventListener("click", (e) => {
-    console.log("eee");
     compList.appendChild(createDiv);
     createDiv.removeChild(compBtn);
     delBtn.addEventListener("click", (e) => {
@@ -20,14 +24,33 @@ function btnClicked() {
       compList.removeChild(createDiv);
     });
   });
-  var createDiv = document.createElement("li"); //variable to create <div></div>
-  createDiv.appendChild(divText); // list(<li></li>) grabs listText(user input) and store it inside
+  //edit below
+  editBtn.addEventListener("click", (e) => {
+    userInput.focus();
+    createDiv.textContent = userInput.value;
+    delBtn.innerHTML = "Delete"; //stores "Delete" into variable button
+    delBtn.classList.add("delete");
+    compBtn.innerHTML = "Complete";
+    compBtn.classList.add("complete");
+    editBtn.innerHTML = "Edit";
+    editBtn.classList.add(`edit-${i}`);
+    createDiv.appendChild(editBtn);
+    createDiv.appendChild(compBtn);
+    createDiv.appendChild(delBtn);
+  });
+  //edit above
   delBtn.innerHTML = "Delete"; //stores "Delete" into variable button
+  delBtn.classList.add("delete");
   compBtn.innerHTML = "Complete";
   compBtn.classList.add("complete");
-  delBtn.classList.add("delete");
+  editBtn.innerHTML = "Edit";
+  editBtn.classList.add(`edit-${i}`);
   createDiv.appendChild(divText);
-  createDiv.appendChild(delBtn); // deleteBtn gets added with the <li></li>
+  createDiv.appendChild(editBtn);
   createDiv.appendChild(compBtn);
+  createDiv.appendChild(delBtn); // deleteBtn gets added with the <li></li>
+
   addTodoList.appendChild(createDiv);
+  i++;
+  userInput.value = "";
 }
